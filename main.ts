28f02be0ef80de8dd16b4f780cc2348d7bcace88
blainefreestone <blainefreestone01@gmail.com>
2024-04-25 21:52:39 -0700

    view is now enterable with ribbon icon

--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, ItemView, WorkspaceLeaf } from 'obsidian';
 
 interface ContributionCalendarSettings {
 	mySetting: string;
@@ -15,10 +13,7 @@ export default class ContributionCalendar extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
+		
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
@@ -27,10 +22,41 @@ export default class ContributionCalendar extends Plugin {
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	
+		this.registerView(
+			CONTRIBUTION_CALENDAR_VIEW_TYPE,
+			(leaf) => new ContributionCalendarView(leaf)
+		);
+
+		const ribbonIconEl = this.addRibbonIcon("dice", "Contribution Calendar", () => {
+			this.activateView();
+		})
+		ribbonIconEl.addClass('contribution-calendar-ribbon-class');
 	}
 
 	onunload() {
 
+	}
+
+	async activateView() {
+		const { workspace } = this.app;
+
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(CONTRIBUTION_CALENDAR_VIEW_TYPE);
+
+		if (leaves.length > 0) {
+			leaf = leaves[0];
+		} else {
+			leaf = workspace.getRightLeaf(false);
+			await leaf?.setViewState({
+				type: CONTRIBUTION_CALENDAR_VIEW_TYPE,
+				active: true
+			});
+		}
+
+		if (leaf !== null) {
+			workspace.revealLeaf(leaf);
+		}
 	}
 
 	async loadSettings() {
@@ -39,5 +65,73 @@ export default class ContributionCalendar extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+}
+
+export const CONTRIBUTION_CALENDAR_VIEW_TYPE = "contributioncalendar-view";
+
+export class ContributionCalendarView extends ItemView {
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	getViewType(): string {
+		return CONTRIBUTION_CALENDAR_VIEW_TYPE;
+	}
+
+	getDisplayText(): string {
+		return "Contribution Calendar";
+	}
+
+	async onOpen() {
+		const container = this.containerEl.children[1];
+		container.empty();
+		container.createEl("h1", { text: "Contribution Calendar" });
+	}
+
+	async onClose() {
+		// Nothing to do
+	}
+}
+
+class SampleModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen() {
+		const {contentEl} = this;
+		contentEl.setText('Woah!');
+	}
+
+	onClose() {
+		const {contentEl} = this;
+		contentEl.empty();
+	}
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: ContributionCalendar;
+
+	constructor(app: App, plugin: ContributionCalendar) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
 	}
 }
